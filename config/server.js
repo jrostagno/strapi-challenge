53a0989c7680e3d8e21e9852c53d6b1c9cb70f7a
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2022-02-23 08:39:27 -0300

    second commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2aa321c26a4f560921370acbc07a36ad'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5222703-e45e-4001-be32-aa09b76246d3'
+};
